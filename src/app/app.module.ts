cb03e19f9456f6423bbba2a50f1996ae0ed24222
MariCarmenBaenaSopra <maricarmenbaena.sopra@gmail.com>
2017-04-04 17:33:16 +0200

    Enrutando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';  /**Enrutamiento */
 
 import { AppComponent } from './app.component';
 import { MovimientosModule } from './movimientos/movimientos.module';
 import { ClientesModule } from './clientes/clientes.module';
+import { ClientesComponent } from './clientes/clientes.component';
+import { HomeComponent } from './home/home.component';
+
+  /**PARA EL ENRUTAMIENTO */
+    const routes: Routes = [
+      { path: '', component: HomeComponent},
+      { path: 'clientes', component: ClientesComponent}
+      ]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     MovimientosModule,
-    ClientesModule
+    ClientesModule,
+    //RouterModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],  /**Injection of common services for the application */
   bootstrap: [AppComponent] /**root component for boot */
