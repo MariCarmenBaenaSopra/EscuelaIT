b67556c10eb90b1e09c11ebeff3e7df619eb5f01
MariCarmenBaenaSopra <maricarmenbaena.sopra@gmail.com>
2017-04-05 12:55:48 +0200

    basic route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { MovimientosComponent } from './movimientos/movimientos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 import { PepeComponent } from './pepe/pepe.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -12,22 +14,28 @@ import { ClientesComponent } from './clientes/clientes.component';
 import { HomeComponent } from './home/home.component';
 import { SaludoComponent } from './saludo/saludo.component';
 
-  /**PARA EL ENRUTAMIENTO */
+  /**PARA EL ENRUTAMIENTO - Definir las rutas */
     const routes: Routes = [
       { path: '', component: HomeComponent },
-      { path: 'clientes', component: ClientesComponent },
-      { path: 'pepe', component: PepeComponent },
+      { path: 'inicio', redirectTo: '' },
+      { path: 'contacto', component: ContactoComponent },
+      { path: 'movimientos', component: MovimientosComponent },
       { path: 'saludo', component: SaludoComponent },
       { path: 'saludo/:amigo', component: SaludoComponent },
-      { path: '**', redirectTo: '', pathMatch: 'full' }   /*Path not found*/
-      ];
+      { /*Path not found*/
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full' 
+      }
+    ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PepeComponent,
-    SaludoComponent
+    SaludoComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
